67a4aa056d28cd52a64964821edc4c14d66443a5
rosalbac4300 <rochu.caldez@gmail.com>
2025-10-31 09:14:10 -0300

    feat: iuse service on init

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,6 @@
 import express, { json } from 'express';
 import cors from 'cors';
+import DatabaseService from './services/database.service.js';
 
 const port = 4000;
 
@@ -7,6 +8,12 @@ const app = express();
 app.use(cors());
 app.use(json());
 
+const databaseService = new DatabaseService();
+
+await databaseService.MigrateAll();
+
 const server = app.listen(port, () => {
 	console.log(`Listening on port ${port}`);
 });
+
+
